9e0856aca79abf79812f036b8edf3c6c34c61b03
Catalin Miron <catalinmiron093@gmail.com>
2025-08-29 12:00:07 +0300

    first commit 222

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "res.cloudinary.com", // Cloudinary
+      "lh3.googleusercontent.com", // Google API (ex: profiluri / imagini Google)
+    ],
+  },
 };
 
 export default nextConfig;
